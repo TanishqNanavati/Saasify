81c4ffb6f04f95239bdd6844d545ed7bd9d8eaae
Tanishq Nanavati <tanishqnanavati3@gmail.com>
2025-10-23 14:03:10 +0000

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "react/no-unescaped-entities": "off",
+    },
   },
 ];
 
